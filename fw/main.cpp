0fc0ebcbf529c8434d6c1bc8546f314438d44b45
Pavel Burgr <pb@drake.cz>
2021-03-08 18:44:30 +0100

    WIP

--- fw/main.cpp
+++ fw/main.cpp
@@ -7,15 +7,36 @@ const int REQUEST_GPIO_READ_INPUT = 3;
 const int REQUEST_GPIO_CONFIGURE_OUTPUT = 4;
 const int REQUEST_GPIO_WRITE_OUTPUT = 5;
 
+int c = 0;
+
 class TestTimer : public genericTimer::Timer {
 
   void onTimer() {
     target::PORT.OUTTGL.setOUTTGL(1 << LED_PIN);
     start(10);
 
-    target::SERCOM0.I2CM.ADDR.setADDR(0x4F << 1);
-    //target::SERCOM0.I2CM.DATA = 0xFF;
+    target::SERCOM0.I2CM.ADDR.setADDR(0x4F << 1); // | 1
+    // target::SERCOM0.I2CM.DATA = 0xFF;
 
+    c = 0;
+  }
+
+public:
+  void interruptHandlerSERCOM() {
+
+    if (target::SERCOM0.I2CM.INTFLAG.getMB()) {
+      target::SERCOM0.I2CM.INTFLAG.setMB(true);
+      if (c++ < 4) {
+        target::SERCOM0.I2CM.DATA = 1;
+      }
+    }
+
+    if (target::SERCOM0.I2CM.INTFLAG.getSB()) {
+      target::SERCOM0.I2CM.INTFLAG.setSB(true);
+      // if (c++ < 4) {
+      // target::SERCOM0.I2CM.INTFLAG.setSB(true);
+      // }
+    }
   }
 };
 
@@ -93,6 +114,8 @@ TestTimer testTimer;
 
 void interruptHandlerUSB() { bridgeDevice.interruptHandlerUSB(); }
 
+void interruptHandlerSERCOM0() { testTimer.interruptHandlerSERCOM(); }
+
 void initApplication() {
 
   atsamd::safeboot::init(9, false, LED_PIN);
@@ -113,17 +136,21 @@ void initApplication() {
   target::PORT.PINCFG[14].setPMUXEN(true);
   target::PORT.PINCFG[15].setPMUXEN(true);
 
-  // SCL at 100kHz for GCLK0 at 8MHz 
+  // SCL at 100kHz for GCLK0 at 8MHz
   target::SERCOM0.I2CM.BAUD = target::SERCOM0.I2CM.BAUD.bare().setBAUD(35).setBAUDLOW(35);
 
-  target::SERCOM0.I2CM.CTRLB = target::SERCOM0.I2CM.CTRLB.bare().setSMEN(true);
+  // target::SERCOM0.I2CM.CTRLB = target::SERCOM0.I2CM.CTRLB.bare().setSMEN(true);
 
   target::SERCOM0.I2CM.CTRLA = target::SERCOM0.I2CM.CTRLA.bare()
-                                   .setSDAHOLD(2)
                                    .setMODE(target::sercom::I2CM::CTRLA::MODE::I2C_MASTER)
+                                   .setSCLSM(true)
                                    .setENABLE(true);
 
-  target::SERCOM0.I2CM.STATUS.setBUSSTATE(1);
+  target::SERCOM0.I2CM.STATUS.setBUSSTATE(1); // force bus idle state
+
+  target::SERCOM0.I2CM.INTENSET = target::SERCOM0.I2CM.INTENSET.bare().setMB(true).setSB(true);
+
+  target::NVIC.ISER.setSETENA(1 << target::interrupts::External::SERCOM0);
 
   testTimer.start(100);
 }
