4081126b05735f7f7f78409cd0238e5cd458b355
Pavel Burgr <pb@drake.cz>
2021-03-30 16:36:33 +0200

    WIP

--- fw/master.cpp
+++ fw/master.cpp
@@ -41,51 +41,96 @@ public:
     sercom->I2CM.STATUS.setBUSSTATE(1); // force bus idle state
   }
 
+  const int CMD_ACK_AND_READ = 2;
+  const int CMD_ACK_AND_STOP = 3;
+
   void interruptHandlerSERCOM() {
 
-    if (sercom->I2CM.INTFLAG.getMB()) {
-      sercom->I2CM.INTFLAG.setMB(true);
+    if (sercom->I2CM.INTFLAG.getMB() || sercom->I2CM.INTFLAG.getSB()) {
+
+      bool read = sercom->I2CM.ADDR & 1;
 
       if (sercom->I2CM.STATUS.getBUSERR() || sercom->I2CM.STATUS.getRXNACK()) {
+        // errors
 
-        if (sercom->I2CM.ADDR & 1) {
+        if (read) {
+          // read
           rxComplete(rxLength);
         } else {
-          txComplete(txLength);
+          // write
+          txComplete(txLength && txLength - 1);
         }
-        
-        sercom->I2CM.CTRLB.setCMD(3);
+
+        sercom->I2CM.CTRLB.setCMD(CMD_ACK_AND_STOP);
 
       } else {
+        if (read) {
 
-        if (txLength < txLimit) {
-          sercom->I2CM.DATA = txBufferPtr[txLength++];
+          // read
+          if (rxLength < rxLimit) {
+            rxBufferPtr[rxLength++] = sercom->I2CM.DATA;
+            sercom->I2CM.CTRLB.setCMD(CMD_ACK_AND_READ);
+          } else {
+            rxComplete(rxLength);
+            sercom->I2CM.CTRLB.setCMD(CMD_ACK_AND_STOP);
+          }
+
         } else {
-          txComplete(txLength);
-          sercom->I2CM.CTRLB.setCMD(3);
+          // write
+          
+          if (txLength < txLimit) {
+            sercom->I2CM.DATA = txBufferPtr[txLength++];
+          } else {
+            txComplete(txLength);
+            sercom->I2CM.CTRLB.setCMD(CMD_ACK_AND_STOP);
+          }
+          
         }
-        
       }
     }
 
-    if (sercom->I2CM.INTFLAG.getSB()) {
-      sercom->I2CM.INTFLAG.setSB(true);
+    // if (sercom->I2CM.INTFLAG.getMB()) {
+    //   //sercom->I2CM.INTFLAG.setMB(true);
 
-      if (rxLength < rxLimit && !sercom->I2CM.STATUS.getRXNACK()) {
-        rxBufferPtr[rxLength] = sercom->I2CM.DATA;
-        rxLength++;
-        if (rxLength < rxLimit) {
-          sercom->I2CM.CTRLB.setCMD(2);
-        } else {
-          rxComplete(rxLength);
-          sercom->I2CM.CTRLB.setCMD(3);
-        }
-      } else {
-        rxComplete(rxLength);
-        sercom->I2CM.CTRLB.setCMD(3);
-      }
-    }
+    //   if (sercom->I2CM.STATUS.getBUSERR() || sercom->I2CM.STATUS.getRXNACK()) {
 
+    //     if (sercom->I2CM.ADDR & 1) {
+    //       rxComplete(rxLength);
+    //     } else {
+    //       txComplete(txLength);
+    //     }
+
+    //     sercom->I2CM.CTRLB.setCMD(3);
+
+    //   } else {
+
+    //     if (txLength < txLimit) {
+    //       sercom->I2CM.DATA = txBufferPtr[txLength++];
+    //     } else {
+    //       txComplete(txLength);
+    //       sercom->I2CM.CTRLB.setCMD(3);
+    //     }
+
+    //   }
+    // }
+
+    // if (sercom->I2CM.INTFLAG.getSB()) {
+    //   //sercom->I2CM.INTFLAG.setSB(true);
+
+    //   if (rxLength < rxLimit && !sercom->I2CM.STATUS.getRXNACK()) {
+    //     rxBufferPtr[rxLength] = sercom->I2CM.DATA;
+    //     rxLength++;
+    //     if (rxLength < rxLimit) {
+    //       sercom->I2CM.CTRLB.setCMD(2);
+    //     } else {
+    //       rxComplete(rxLength);
+    //       sercom->I2CM.CTRLB.setCMD(3);
+    //     }
+    //   } else {
+    //     rxComplete(rxLength);
+    //     sercom->I2CM.CTRLB.setCMD(3);
+    //   }
+    // }
   }
 
   virtual void rxComplete(int length){};
@@ -95,12 +140,16 @@ public:
     rxLength = 0;
     rxLimit = length > rxBufferSize ? rxBufferSize : length;
     sercom->I2CM.ADDR.setADDR(address << 1 | 1);
+    while (sercom->I2CM.SYNCBUSY)
+      ;    
   }
 
   virtual void startTx(int address, int length) {
     txLength = 0;
     txLimit = length > txBufferSize ? txBufferSize : length;
     sercom->I2CM.ADDR.setADDR(address << 1);
+    while (sercom->I2CM.SYNCBUSY)
+      ;    
   }
 };
 } // namespace atsamd::i2c
